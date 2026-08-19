4f89acbd4fcb2f61968b6e2fc8d1a567e811f8dc
institucioneducativabetania <betaniadeimer@gmail.com>
2026-08-18 21:26:24 -0500

    Publicar primera noticia con imagencarpeta noticias

--- assets/data/noticias.js
+++ assets/data/noticias.js
@@ -1,5 +1,20 @@
 window.NOTICIAS_IEB = [
 
+
+
+{
+  fecha: "2026-08-18",
+  fechaTexto: "18 de agosto de 2026",
+  categoria: "Actividad institucional",
+  titulo: "Actividad de la Institución Educativa Betania",
+  resumen: "La comunidad educativa participó en una actividad orientada al fortalecimiento de los procesos institucionales y la formación integral de los estudiantes.",
+  imagen: "assets/img/noticias/2026/jornada-pae-01.jpg",
+  enlace: "",
+  textoEnlace: "Ver más"
+},
+
+
+
   {
     fecha: "2026-08-18",
     fechaTexto: "18 de agosto de 2026",
